56f6206092299de7fece33e2eff373a7cbb4052d
Ian Fox <ianfox97@gmail.com>
2020-05-16 18:20:33 -0400

    Use google text to speech for output

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # terriblehack-podcasts
+
+Re-record your podcasts with less annoying voices.
+
+`./convert.py test.wav converted.mp3`
--- convert.py
+++ convert.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 
 import speech_recognition as sr
-# from pathlib import path
 import sys
 
 input_file = sys.argv[1]
+output_file = sys.argv[2]
 
 r = sr.Recognizer()
 with sr.AudioFile(input_file) as source:
     audio = r.record(source)
+
+text = ""
 
 # recognize speech using Sphinx
 try:
     text = r.recognize_sphinx(audio)
 except sr.UnknownValueError:
     print("Error: sphinx could not understand audio")
+    sys.exit(1)
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
+    sys.exit(1)
+
+# Print transcript
+print(text)
+
+# Speak text with google text to speech
+from gtts import gTTS
+tts = gTTS(text)
+tts.save(output_file)
